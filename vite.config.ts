a78a8668340d531246c6f43b456954db2c924104
Maryam Khan <markha@kth.se>
2026-01-30 10:18:41 +0100

    minor change

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  cacheDir: '/tmp/.vite',
   preview: {
     allowedHosts: true,
   },
